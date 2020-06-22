787833cbb872359066d2bc232d34d04efa668f5d
Mohammed Abunada <m7606225@gmail.com>
2020-06-22 03:53:16 +0200

    Initial Commit

---
+++ AlAqsaExpandingView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlAqsaExpandingView
+//
+//  Created by Mohammed Abunada on 2020-06-22.
+//  Copyright © 2020 NMDS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
